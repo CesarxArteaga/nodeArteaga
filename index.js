0ed14c0157f710864678889c1afa11fe5bcf87f7
CesarxArteaga <ctmces94@gmail.com>
2018-08-21 10:37:32 -0500

    www

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+//var express = require('express');
+
+var expresshbs = require('express-handlebars');
+
+var app = express();
+
+
+
+app.engine('handlebars', expresshbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+/*
+app.get('/', function (req, res){
+	res.render('home');
+});*/
+
+app.get('/', (req, res) => res.render('home'));
+
+app.listen(PORT , () => console.log('localhost on ${PORT}'));
